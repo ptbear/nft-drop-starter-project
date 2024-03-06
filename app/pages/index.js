a45f06b93b26ad718b359221a805d38604bbce0c
ptbear <ptbear@gulu.local>
2024-03-06 16:38:11 +0800

    a

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,19 +1,40 @@
 import React from "react";
+import dynamic from "next/dynamic";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+const WalletMultiButtonDynamic = dynamic(
+    async () => (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    { ssr: false }
+);
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home = () => {
+    const wallet = useWallet();
+    console.log(wallet.publicKey);
+
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+            <div className="button-container">
+                <WalletMultiButtonDynamic className="cta-button connect-wallet-button"/>
+            </div>
+        </div>
+    );
     return (
         <div className="App">
             <div className="container">
                 <div className="header-container">
                     <p className="header">🍭 Candy Drop</p>
                     <p className="sub-text">NFT drop machine with fair mint</p>
+                    {/* Render your connect to wallet button right here */}
+                    {/*renderNotConnectedContainer()*/}
+                    {wallet.publicKey ? "hello" : renderNotConnectedContainer()}
                 </div>
                 <div className="footer-container">
-                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+                    <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
                     <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
                 </div>
             </div>
